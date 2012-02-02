5148b5f7b0e47758add1b2740dbfaae272c2e807
Jukka Svahn <web@rahinaa.biz>
2012-02-02 20:16:11 +0200

    Adding v0.5 from SVN.

--- code.php
+++ code.php
@@ -1,7 +1,7 @@
 <?php	##################
 	#
 	#	rah_textile_bar-plugin for Textpattern
-	#	version 0.4
+	#	version 0.5
 	#	by Jukka Svahn
 	#	http://rahforum.biz
 	#
@@ -9,6 +9,7 @@
 
 	if (@txpinterface == 'admin') {
 		register_callback('rah_textile_bar','admin_side','head_end');
+		register_callback('rah_textile_bar_head','admin_side','head_end');
 		add_privs('rah_textile_bar', '1,2');
 		register_tab("extensions", "rah_textile_bar", "Textilebar");
 		register_callback("rah_textile_bar_page", "rah_textile_bar");
@@ -21,6 +22,39 @@
 		if(gps('rah_textile_bar_css'))
 			rah_textile_bar_css();
 	}
+
+/**
+	Adds CSS styles to the <head> for the panel
+*/
+
+	function rah_textile_bar_head() {
+		global $event;
+		
+		if($event != 'rah_textile_bar')
+			return;
+		
+		echo
+			<<<EOF
+				<style type="text/css">
+					#rah_textile_bar_container {
+						width: 950px;
+						margin: 0 auto;
+					}
+					.rah_textile_bar_items label {
+						float: left;
+						width: 150px;
+					}
+					.rah_textile_bar_clear {
+						clear: both;
+						height: 15px;
+					}
+				</style>
+EOF;
+	}
+
+/**
+	Installer
+*/
 
 	function rah_textile_bar_install() {
 		safe_query(
@@ -36,20 +70,32 @@
 			safe_insert("rah_textile_bar","name='fields', value='excerpt,body'");
 	}
 
+/**
+	Delivers the panels
+*/
+	
 	function rah_textile_bar_page() {
 		global $step;
 		require_privs('rah_textile_bar');
-		if(in_array($step,array(
-			'rah_textile_bar_save'
-		))) $step();
-		else rah_textile_bar_edit();
+		if($step == 'rah_textile_bar_save')
+			rah_textile_bar_save();
+		else
+			rah_textile_bar_edit();
 	}
+
+/**
+	Saves the preferences
+*/
 
 	function rah_textile_bar_save() {
 		rah_textile_bar_update('not','disable');
 		rah_textile_bar_update('fields','fields');
 		rah_textile_bar_edit('Preferences saved');
 	}
+
+/**
+	Runs update query
+*/
 
 	function rah_textile_bar_update($value='',$field='') {
 		$value = ps($value);
@@ -61,6 +107,10 @@
 			"value='$value'","name='$field'"
 		);
 	}
+
+/**
+	The preferences panel
+*/
 
 	function rah_textile_bar_edit($message='') {
 		pagetop('rah_textile_bar',$message);
@@ -95,49 +145,42 @@
 			'#textilebar .output_form',
 			'#textilebar .acronym'
 		);
-		echo 
-			'	<link href="'.hu.'?rah_textile_bar_css=1" rel="Stylesheet" type="text/css" />'.n.
-			'	<style type="text/css">'.n.
-			'		.rah_textile_bar div {'.n.
-			'			border: 1px solid #ccc !important;'.n.
-			'		}'.n.
-			'	</style>'.n.
-			'	<form method="post" action="index.php" style="width:950px;margin:0 auto;">'.n.
+		$out[] = 
+			n.
+			'	<form method="post" action="index.php" id="rah_textile_bar_container">'.n.
 			'		<h1><strong>rah_textile_bar</strong> | Simple Textile Inserting Bar</h1>'.n.
 			'		<p>&#187; <a href="?event=plugin&amp;step=plugin_help&amp;name=rah_textile_bar">Documentation</a></p>'.n.
-			'		<p><input type="submit" value="Save settings" class="publish" /></p>'.n.
 			
-			'		<fieldset class="rah_textile_bar">'.n.
-			'			<legend>Add textile bar to</legend>'.n.
+			'		<p><strong>Add textile bar to</strong></p>'.n.
 			'			<p>'.n.
 			'				<label><input type="checkbox" name="fields[]" value="excerpt"'.((in_array('excerpt',$field)) ? ' checked="checked"' : '').' /> Excerpt</label>'.n.
 			'				<label><input type="checkbox" name="fields[]" value="body"'.((in_array('body',$field)) ? ' checked="checked"' : '').' /> Body</label>'.n.
 			'			</p>'.n.
-			'		</fieldset>'.n.
 			
-			'		<fieldset class="rah_textile_bar">'.n.
-			'			<legend>Disable and remove items from the Textile bar. Check a item to disable it.</legend>'.n.
-			'			<table style="width:100%" cellspacing="0" cellpadding="0" border="0" class="list" id="list">'.n.
-			'				<tr>'.n.
-			'					<th>Icon</th>'.n.
-			'					<th>Item name</th>'.n.
-			'					<th>Disabled?</th>'.n.
-			'				</tr>'.n;
-		foreach($items as $item) {
-			echo 
-			'				<tr>'.n.
-			'					<td><div class="'.str_replace('#textilebar .','',$item).'"></div></td>'.n.
-			'					<td><label for="'.str_replace('#textilebar .','',$item).'">'.str_replace('#textilebar .','',$item).'</label></td>'.n.
-			'					<td><input type="checkbox" id="'.str_replace('#textilebar .','',$item).'" value="'.$item.'" name="not[]"'.((in_array($item,$not)) ? ' checked="checked"' : '').' /></td>'.n.
-			'				</tr>'.n;
-		}
-		echo 
-			'			</table>'.n.
-			'		</fieldset>'.n.
+			'		<p><strong>Disable and remove items from the Textile bar.</strong> Check a item to disable it.</p>'.n.
+			'		<p class="rah_textile_bar_items">'.n;
+
+		foreach($items as $item) 
+			$out[] = 
+				'		<label>'.n.
+				'			<input type="checkbox" id="'.str_replace('#textilebar .','',$item).'" value="'.$item.'" name="not[]"'.((in_array($item,$not)) ? ' checked="checked"' : '').' />'.n.
+				'			'.str_replace('#textilebar .','',$item).n.
+				'		</label>'.n;
+		
+		$out[] = 
+			'		</p>'.n.
+			'		<div class="rah_textile_bar_clear"></div>'.n.
+			'		<p><input type="submit" value="Save settings" class="publish" /></p>'.n.
 			'		<input type="hidden" name="event" value="rah_textile_bar" />'.n.
 			'		<input type="hidden" name="step" value="rah_textile_bar_save" />'.n.
 			'	</form>'.n;
+		
+		echo implode('',$out);
 	}
+
+/**
+	Adds the required scripts to Write panel's head
+*/
 
 	function rah_textile_bar() {
 		global $event;
@@ -147,23 +190,37 @@
 				'	<link href="'.hu.'?rah_textile_bar_css=1" rel="Stylesheet" type="text/css" />'.n.n;
 	}
 
+/**
+	Generates a button
+*/
+
 	function rah_textile_bar_button ($class='',$name='',$start='',$close='',$not=array()){
 		if(!in_array('#textilebar .'.$class,$not))
 			return '	theButtons[theButtons.length] = new edButton('."'".$class."','".$name."','".$start."','".$close."');".n;
 	}
+
+/**
+	Hooks the JavaScript to certain elements
+*/
 	
 	function rah_textile_bar_load_fields() {
 		$out = array();
 		$fields = fetch('value','rah_textile_bar','name','fields');
-		$fields = explode(',',$fields);
-		if(count($fields) == 0)
+		
+		if(empty($fields))
 			return;
+		
+		$fields = explode(',',$fields);
 		foreach($fields as $key => $field) 
 			$out[] = 
 				'		$("textarea#'.$field.'").before(\'<div class="rah_textile_bar" id="rah_textile_bar_'.$key.'"></div>\');'.n.
 				'		addEvent(window, "load", function() {initQuicktags("'.$field.'","rah_textile_bar_'.$key.'")});'.n;
 		return implode('',$out);
 	}
+
+/**
+	Delivers the JS
+*/
 
 	function rah_textile_bar_js() {
 		ob_start();
@@ -204,6 +261,10 @@
 			'	});';
 		exit();
 	}
+
+/**
+	Delivers the CSS
+*/
 
 	function rah_textile_bar_css() {
 		ob_start();
@@ -305,6 +366,10 @@
 		exit();
 	}
 
+/**
+	Delivers the image
+*/
+
 	function rah_textile_bar_img() {
 		ob_start();
 		ob_end_clean();
@@ -312,4 +377,4 @@
 		$code = 'R0lGODlhEAB6AocAAAAAAAwMDBQUFBwcHCUlJSsrKzMzMzk5OUREREdHSEtLS01WTFNTU1tbW011S1FiUFdwV1xtdWF+VGNjY2pqamVrdW53aGpxenZ+bnNzc3J0enZ+dXh8dXt7e218i0t811J81zuCNz/Rd0CGPEWDQk+cSFGUTFC8WVepe2eSRWCRWHaCXmOLYWSJdmuZaGejWmCwVnamS3q6T368VWysZWu/YHqoaHeucny4dErVZ13TbGrbdnTGaH7Lc3fWdl+In16RikyArUeCsFmIuF+pr2aYlXeBhmqXpHiqn2e8vH+zoU2K3FSJ3FOZxleS3FmZ4l2nyF6w01ql5mSJ3G2WwGOV3nKawnie1mWa4XWa4WmlwWWl32u2yXyhxn2i2WWn5mm163am43S06m/DgmzRkHPegnHWmm3EtGDYpn/FvXrRr3XJy4RzTYJ9dYaMQoCDVoC+Uoe5aIS9fojBV5DFXY/FY4nEdJjJZpHHeqDNfajSdIKDgoaHioKOgYKIi4qHgouLi42RjJOTk5ubm4u7g5emiJaolIC1poe1t6SkpKqrqqi7p7S0tLq6uoepzIao3I200JaszpWr0Jmy0YWk4oKu5oqs5IK15oOw6IO76Ii054y26Yi454266pS045Sz6JS765m05Zm06J255p2866W41KW96orDhIPYgZLIiZDThJvZk4TLtYrbu43qnZzivaTIiKHLk6XThaXWlbXemKzXpq/VvbTeoLfXvKLim6fvsazwtbvhpbTvtY7ByYvL15DN25jV2YnB6pfE66rC2qjXy6bZ17/CybPG3bTaxbXe1qPC7aLN76zD7KrM7qfW8bLH67XO8b7Q47rV8qnuybrmw7fo1MLbpcLcv83nr83ns9bst9zwvsPDw8DFysrKysfexM7T09TU1NXX2Nzc3MvX5sfZ89Hd88rp0NPtztbt09ryw9zx2cXk9trk79jm+uX0xuTk5Orq6uXt+OXw/uzx9uvx+/Pz9PH1+/X69fT4/fj89/v8/P///wAAAAAAACH5BAMAAP0ALAAAAAAQAHoCAAj/APsJHEiwoMGDCBMqXMiwocOHECMeVAQoULd+3fZ0GIRxT0EKfAZ2UNAvHAUDBSeEFNghgcBBBVJmUJQoUQMELwkUbOBRYIYDAgXpJMigZ7+TQQcUZNBhIIWY/QIpHTioQ4ZG/RhN4KloAoMM5CSKHUu2rNmzaNOqXcu2rVu1FC123EiuA4WLTlf2aymvQaAGBeINVCnyACPB3wrg7TdhZs2bgwvcG8hz4E+WDcYRNYp0Y797ggUydUrgm4EGDAy/tIpVawMKDGJPeEu7tu3buHPr3v0w7sWMdO0uPqp3JGNBgAkSZulScCIGyh3bxBlvAgFBBCv7BNovngICjAYW/3VqYFyi7gMAiW8qkMJ1At3EDcBb9WrWrRz2NA7Pu7///wAGKGCAvmHExx4c9SNPIARRoJ5Ae0D32QSzOfXgXiRhWKFAjSni4QQ47fHNVsoZ9dM3CExggAFGadfPT/d8840CCYhDGXtHoSQQagMl0gEHrVEwQU8aDWjkkUgmqWSSBQI3yCAURIlVe8WRlEGUE0zGYXEuNaBIaINJB5kBBRCAYz8uXtbPOIIMoIh4nJUnyGQZXDhae+9hxQB9rN3XAAcZTNDBlEsWauihiCbqUJMaJRjOHmCCJBJJ91CQgZZbitQlBQZ1+NgB3RSQgQIUYJpmATChl2A/47VXgCBQpf+3nlMFiAPfOPOtZp9rezRCIX+KBivssMT6940gGXTAiCAUDIJpQY1QwGA/gzDA6UGMSNqPIgrghG1l9zSQgFEFMfJaIhQgcO1EDHAA4QEKPNujAhkIdA8BqB6USALrDlCAXgQlckADOQ0A5kCMBMrTSB2EVezDEEd8mz2ddLIMQ6DYswwmDlUSBkGDCCJIIPJoVC8pYbxDkDiBnqcIH/GYYwk+5sxD0DhbKVJvP2FggUUVBiVCo7wH3ZMAAhcmNNMBBgxX0D2AvMmHAQTYeNCVgIzTgAI0hiPx12CHLTbIIpNsMkIsT+AyzAnh7OXOCgmdANEGGY00Q0s33Q/FFhP/BLXUVIuT8TKVEIS11lwn4LXHDaGs8kLmUKJPzQv1/PPYmGc+kMYOLXNx5wJ9k8HoUTbAo0Cef64WKaTQ45A9mzQ0jyYCNbJ1t0yXORAmjK8Vxu8OpezQPB8rUuYAAggQQAAADBRGFp9oLv30EE+zzDQNLRPNxaKTTuHp/ViP/X/TkBJNQ6Bs3I/tXCNwwAG6CxRNJc0A2EwWqisUxibFH588880TiCnCYArqGfCAbWFHLnrAAx70YBXpMAg/csEDXmijGsmwRS1qkIp9DIQfqpAFPNiRDmsYwxjBsEUqcJAPgcxiFdxYhzrQYQxrAOMXvrAFHuTQD3bwgBu92EU1/6wRjGBwgQusWEU2SgCOVdBCG7qgBjpuGIUkmKEMZZAFLG7Qg23kwhXWQMcR17CDHORABzi4hgl+iIodvKIYUEhCK9AgghycgAbZGMEPe0CGV0AhCk3gghrOQAQUvCAbIehBNlTRijT8wA+DMEIQhGCEQfyhECRIBS1uMYYWkIwc3TCCB8gBSkFAQB0w2IYKMhCOCChgAoLgwAQUgIFuNOUUdrDAHsKhBQQ0IhzdUAQCbNCNP/QjHzSQACsjgAAEMIBGCHhAN3a2DxcsIBCKWFYGOCAIRiiCWQRZxAImECUHsOBKQnIYAtdZqHKUYhKRkIQk4hlPeUaiHAIphTzugf+PfNxDH/f45z3qUY9JCGQS9/AEJA4RC1nIIhVF6MIV5mHQfkgiH6FARCz0cIeOEuIIVyioQC4aCiXI4g50mMMc8AAEL9QjEiPFxyiUkIeUwgEOcQDCIygqkEjIFBGpqAMcZDCDGxxhpxWVRD1IMQkkxOGpNojoI14aU1OMAhIrcIMb3uAIS0w1qfWIgVjZ8IdBtCEFKWjDIADRiH0oFRvY6MMnu8GHQIyjlIN4qyE6EI4NkApZFFDABWypT3wAgg/hkEcCfhnMBESCruUgxiQ8QIFwVAABCXjm0TIwzYGQgwLdVMQgMqCBQWQTnJ4lp6UawbIrTUAz7IytbCGiD4f/6EMMYBAIGMRQW4PgQwxi+IJugYuPgkwDDGD4guv2tlspnG8gXxCDFB4nkHdIQQxP4MdAoiGFL2BhufT4wheeAI2C4AMLXwBaP8SLheL6tgpOEMgTsNDb2dr3vorSxzAyMYz9ZsJmCGnHMNrRjkxQ9yDCeEYmhHEJeyTkGdPIxDkYbI7gincLH+vHPITBjHO8gxlhMMcluguGLMSuH5x4hjOGcb1KzGMLUnjCFirBD344wxmXaAY0QkGKSpBCCliYxhMo0Y93XGIY56juI8LwhCeYIxpPyEI/OjEMTXiiH+cgYBWkYIl+QHkKCGlGd5/AhPlOwb0EOQeZsyAKUUwB/wtTOLBAsiAFyQkEGiCIc5pBwYQlQEO786DEB0BQwIEs4dBLmEJtQfCBRn8Av5CONEPwIYkhWPrSlnYEgAUCCWLU+NOflgYVtCuQIfBDH/RIdarf8Q5+dGHTo7bHqunB6lY7Ap8CGbWqac3qdvDDEUkuNT9kXet3tKPVXXDHQBzh6U/Tmh78QIYVSN0PfEzi0pOoMT5KYYVNG6TbpRgCMRTyziFIYyH1oMK5Jc1uYcVDRvCOtzpDtw9Qf/oe3Zj3N/gR0H0E9B7y4Ec8ugGmfe8DoP6WR8D9ne/Q9ZMfB/dnwD/9DYF4A7f82K09AqrwePCj4viI7hcyDtyNc/zjx/9NLrTnEV3n/vvjLT/Hp62L3Xvw++Pc9e48+EEPLIDhCc24+cer7YT08gO9WLBHjW3OD7x4A778mC/T613vivdj3/bOutWxnnVQb73r9rb6u2XUjXiXXUYHa7faMfeNPWQAWMgBFsjg9hm6EwRZBXHXQVBrGYQAYkPtQQgfCKYchERoJwgZhAIW03aEyEMBDUBMAtJOkHh0wCu7RIv/lMe8tXs+tvlQRDcaYUmFKEsgjJDQQbpBgTexpG1bEURYrC742CRgArQfyDQ5oAFATeAAAwCA8AGQtM8MRBEJMECGDMKI8wxkANxh/gEwRYBpGUQRB2CAIBIhiDN9/vvgV8vFPRLxJXkoZPyJGEQi2IqQ8TNiEJqpzkH2kQhvWi0e6xpIPkzLCHHwI2qD9zSDwAiM8A1/AyXPcg8DyAjhsA8dwH0NYH4D4X6NMA780AHfxAASaC+C8CaJsAetJwgMIC/kMCcCIXqCkCUGUTJaEg+BMAEbSBD7EDLxIA4UEIEJkQ/hkAFeQjfh94OxlTANICh40wCEkhDxwACEtxDVUnwIMQEJ4DQHQQ5H0xCCMC4NwQAGcIQIEQ5kwhCDYDoMAHhAWIa3ERAAOw==';
 		echo base64_decode($code);
 		exit();
-	}?>
+	}
